ee6e05ac74625a4914ac4821f8caeb08924276cf
Sackstools <106899719+Sackstools@users.noreply.github.com>
2024-04-07 21:37:51 -0300

    comment

--- GLKLib/stdafx.cpp
+++ GLKLib/stdafx.cpp
@@ -4,5 +4,15 @@
 
 #include "stdafx.h"
 
+// The 'stdafx.h' header file is a precompiled header file that is included at the beginning
+// of this source file. It contains standard system and project-specific include files
+// that are required for the compilation of this source file.
+
+// The precompiled header file is used to improve compile times by allowing the compiler
+// to skip processing the included headers that have not changed since the last compilation.
+// The precompiled header file is compiled into a binary file (.pch) that is then linked
+// with this source file during compilation.
+
 // TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
+// and not in this file. Any additional headers that are required for this source file
+// should be added to the STDAFX.H header file.
