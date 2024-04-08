bf3c3683a3fd4b8fa2d6df002019e76c2acebbfe
Sackstools <106899719+Sackstools@users.noreply.github.com>
2024-04-07 21:37:48 -0300

    comment

--- GLKLib/GLKGLList.h
+++ GLKLib/GLKGLList.h
@@ -1,19 +1,26 @@
 // GLKGLList.h: interface for the GLKGLList class.
-//
+// This header file declares the GLKGLList class, which is derived from the GLKObject class.
+// The GLKGLList class is intended to be used for managing and rendering OpenGL objects.
 //////////////////////////////////////////////////////////////////////
 
-#ifndef _GLKGLLIST
+#ifndef _GLKGLLIST // Include guard to prevent multiple inclusions of this header file
 #define _GLKGLLIST
 
-#include "GLKLib.h"
-#include "GLKObList.h"
+#include "GLKLib.h" // Include GLKLib.h for required functionality
+#include "GLKObList.h" // Include GLKObList.h for required functionality
 
-class GLKGLList : public GLKObject  
+class GLKGLList : public GLKObject // Declare GLKGLList as a class derived from GLKObject
 {
 public:
-	GLKGLList() {};
-	virtual ~GLKGLList() {};
+    // Default constructor for GLKGLList class
+    GLKGLList() {};
+
+    // Destructor for GLKGLList class
+    virtual ~GLKGLList() {};
+
+    // A virtual function to draw the OpenGL objects managed by this class
+    // This function takes a GLKLib pointer as an argument, which represents the OpenGL view
     virtual void draw(GLKLib *view) {};
 };
 
-#endif 
+#endif  // Include guard
