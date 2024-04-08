30791b25fe45dac457333d08041fa9d62a3a62a4
Sackstools <106899719+Sackstools@users.noreply.github.com>
2024-04-07 21:37:49 -0300

    comment

--- GLKLib/GLKNearestNeighbor.h
+++ GLKLib/GLKNearestNeighbor.h
@@ -1,37 +1,34 @@
 // GLKNearestNeighbor.h: interface for the GLKNearestNeighbor class.
+// This header file defines the GLKNearestNeighbor class, which is used for finding the nearest neighbor in a set of points.
 //
 //////////////////////////////////////////////////////////////////////
 
 #ifndef _GLKNEAREST_NEIGHBOR
 #define _GLKNEAREST_NEIGHBOR
 
-#include "GLKObList.h"
+#include "GLKObList.h" // Include GLKObList header file
 
-class GLKNearestNeighborNode : public GLKObject
+class GLKNearestNeighborNode : public GLKObject // GLKNearestNeighborNode class inherits from GLKObject class
 {
 public:
-	float m_pos[3];
-	int m_index;
-	GLKPOSITION m_PosInGLKObList;
+	float m_pos[3]; // Array of 3 floats to store the position of the node
+	int m_index; // Index of the node
+	GLKPOSITION m_PosInGLKObList; // Position of the node in the GLKObList
 };
 
-class GLKNearestNeighbor  
+class GLKNearestNeighbor // GLKNearestNeighbor class
 {
 public:
-	GLKNearestNeighbor(int pntNum, float* xpos, float *ypos, float *zpos);
-	GLKNearestNeighbor(int pntNum, float** pnts);
-	virtual ~GLKNearestNeighbor();
+	GLKNearestNeighbor(int pntNum, float* xpos, float *ypos, float *zpos); // Constructor with 4 parameters
+	GLKNearestNeighbor(int pntNum, float** pnts); // Overloaded constructor with 3 parameters
+	virtual ~GLKNearestNeighbor(); // Destructor
 
-	void PntsInRange(float queryPnt[], float range, GLKArray* indexArray);
-	void MovePnt(int pntIndex, float xNewPos, float yNewPos, float zNewPos);
+	void PntsInRange(float queryPnt[], float range, GLKArray* indexArray); // Function to find points within a certain range of a query point
+	void MovePnt(int pntIndex, float xNewPos, float yNewPos, float zNewPos); // Function to move a point to a new position
 
 private:
-	int m_xNum,m_yNum,m_zNum;
-	float m_size,m_xmin,m_ymin,m_zmin,m_xmax,m_ymax,m_zmax;
-	GLKObList ****m_grids;
+	int m_xNum,m_yNum,m_zNum; // Integer variables to store the number of points in each dimension
+	float m_size,m_xmin,m_ymin,m_zmin,m_xmax,m_ymax,m_zmax; // Floating point variables to store the size and range of the points
+	GLKObList ****m_grids; // 4D array of GLKObList objects
 
-	int m_pntNum;
-	GLKNearestNeighborNode **m_nodeArray;
-};
 
-#endif
