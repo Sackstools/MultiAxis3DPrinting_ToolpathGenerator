c93b830abc7e26e60f19626fda07973b31f27eb3
Sackstools <106899719+Sackstools@users.noreply.github.com>
2024-04-07 21:38:12 -0300

    comment

--- GLKLib/InteractiveTool.h
+++ GLKLib/InteractiveTool.h
@@ -1,82 +1,83 @@
+// InteractiveTool.h
+
+/**
+ * @file InteractiveTool.h
+ * @brief Header file for the InteractiveTool class.
+ * @details This file contains the declaration of the InteractiveTool class, which is a derived class from GLKMouseTool.
+ * The class is used to handle mouse events for selecting nodes, edges, faces, fixed points, and handles in a 3D scene.
+ */
+
 #ifndef INTERACTIVETOOL_H
 #define INTERACTIVETOOL_H
 
-#include "GLKLib.h"
-#include <QPoint>
+#include "GLKLib.h" // Include GLKLib library
+#include <QPoint> // Include QPoint class from the Qt GUI library
 
+/**
+ * @enum select_type
+ * @brief Enum to represent the type of selection.
+ * The enum has the following values:
+ * - NODE: Select nodes
+ * - EDGE: Select edges
+ * - FACE: Select faces
+ * - FIX: Select fixed points
+ * - NHANDLE: Select handles
+ */
 typedef enum select_type { NODE, EDGE, FACE, FIX, NHANDLE };
 
+/**
+ * @class InteractiveTool
+ * @brief Class to handle mouse events for selecting nodes, edges, faces, fixed points, and handles in a 3D scene.
+ * The class is derived from GLKMouseTool and has methods to process mouse events and select nodes, edges, faces, fixed points, and handles.
+ */
 class InteractiveTool : public GLKMouseTool
 {
 public:
-    InteractiveTool(GLKLib *pGLKLib, QMeshPatch *meshPatch_,  GLKMouseTool *mouseTool, select_type selectType, bool Select);
-    InteractiveTool(GLKLib *pGLKLib, GLKObList *polygenMeshList,  GLKMouseTool *mouseTool, select_type selectType, bool Select);
+    /**
+     * @brief Constructor for the InteractiveTool class with a QMeshPatch.
+     * @param pGLKLib Pointer to the GLKLib object.
+     * @param meshPatch_ Pointer to the QMeshPatch object.
+     * @param mouseTool Pointer to the GLKMouseTool object.
+     * @param selectType The type of selection.
+     * @param Select True if the tool is used for selection, false otherwise.
+     */
+    InteractiveTool(GLKLib *pGLKLib, QMeshPatch *meshPatch_, GLKMouseTool *mouseTool, select_type selectType, bool Select);
+
+    /**
+     * @brief Constructor for the InteractiveTool class with a GLKObList.
+     * @param pGLKLib Pointer to the GLKLib object.
+     * @param polygenMeshList Pointer to the GLKObList object.
+     * @param mouseTool Pointer to the GLKMouseTool object.
+     * @param selectType The type of selection.
+     * @param Select True if the tool is used for selection, false otherwise.
+     */
+    InteractiveTool(GLKLib *pGLKLib, GLKObList *polygenMeshList, GLKMouseTool *mouseTool, select_type selectType, bool Select);
+
+    /**
+     * @brief Destructor for the InteractiveTool class.
+     */
     virtual ~InteractiveTool();
 
-    virtual int process_event(QEvent *event, mouse_event_type even_type);
+    /**
+     * @brief Process mouse events and handle the selection.
+     * @param event Pointer to the QEvent object.
+     * @param event_type The type of mouse event.
+     * @return The result of the event processing.
+     */
+    virtual int process_event(QEvent *event, mouse_event_type event_type);
 
+    /**
+     * @brief Get the type of selection.
+     * @return The type of selection.
+     */
     select_type getSelectedType() {return type;};
 
 private:
-    GLKLib *pGLK;
-    QMeshPatch *meshPatch;
-    GLKObList *polygenList;
-    select_type type;
-    GLKMouseTool *mouse_tool;
+    GLKLib *pGLK; // Pointer to the GLKLib object
+    QMeshPatch *meshPatch; // Pointer to the QMeshPatch object
+    GLKObList *polygenList; // Pointer to the GLKObList object
+    select_type type; // The type of selection
+    GLKMouseTool *mouse_tool; // Pointer to the GLKMouseTool object
 
-    QPoint lastPos;
+    QPoint lastPos; // The last position of the mouse cursor
     QPoint pos;
-
-	bool isSelect = true;
-
-    bool bAltPressed;
-    bool bShiftPressed;
-
-    void _selectNodes(QEvent *event, mouse_event_type event_type);
-    void _selectEdges(QEvent *event, mouse_event_type event_type);
-    void _selectFaces(QEvent *event, mouse_event_type event_type);
-	void _selectFixed(QEvent *event, mouse_event_type event_type);
-	void _selectHandle(QEvent *event, mouse_event_type event_type);
-
-
-    void GetVisibleMesh(QMeshPatch *patch);
-};
-
-//#include "GLKLib.h"
-
-//class QMeshPatch;
-//class QMeshNode;
-//class QMeshFace;
-//class InteractiveEntity;
-
-//typedef enum select_type {NODE, FACE, NODECLUSTER, FACECLUSTER};
-
-//class InteractiveTool : public GLKMouseTool
-//{
-//public:
-//    InteractiveTool(GLKLib *pGLKLib, QMeshPatch *patch, GLKMouseTool *cameraTool, select_type selectType);
-//    virtual ~InteractiveTool();
-
-//    virtual int process_event(QEvent *event, mouse_event_type event_type);
-
-//    GLKLib *pGLK;
-//    QMeshPatch *m_patch;
-
-//private:
-//    GLKMouseTool *tool;
-//    select_type type;
-
-//    void _selectNode(QEvent *event, mouse_event_type event_type);
-//    void _selectFace(QEvent *event, mouse_event_type event_type);
-
-//    int m_totalNodeNum;
-//    InteractiveEntity **m_node;
-//    void _addNodeToDrawList();
-//    void _deleteNodeFromDrawList();
-
-//    QMeshFace* CalProjectedTrgl(double xx, double yy, double zz,
-//               double nx, double ny, double nz, double& t,double& u,double& v,double& w);
-//};
-
-#endif // INTERACTIVETOOL_H
-
